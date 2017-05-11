76f956434f715e4ff10452285c72a1b35d0fcea1
Abdul Wahab <cs.abdulwahab@gmail.com>
2017-05-11 15:29:14 +0500

    S17 UOL MAD Sec B Broadcast Receiver

---
+++ app/src/main/java/com/example/abdul_wahab/uolmads17secbbroadcastreceiver/MyReceiver.java
@@ -0,0 +1,20 @@
+package com.example.abdul_wahab.uolmads17secbbroadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class MyReceiver extends BroadcastReceiver {
+    private static final String TAG = "MTAG";
+
+    public MyReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Log.d(TAG, "onReceive: in the receiver ");
+
+    }
+}
