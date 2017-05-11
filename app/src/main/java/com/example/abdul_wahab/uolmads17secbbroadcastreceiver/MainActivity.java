e0e0581e904ed4aa103753e8aa40eff204b5b5cf
Abdul Wahab <cs.abdulwahab@gmail.com>
2017-05-11 15:32:49 +0500

    Query  the  Package Manager for Broadcast Receivers registered for SMS Receiver
    and we found 3 receivers with priority 0

--- app/src/main/java/com/example/abdul_wahab/uolmads17secbbroadcastreceiver/MainActivity.java
+++ app/src/main/java/com/example/abdul_wahab/uolmads17secbbroadcastreceiver/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.abdul_wahab.uolmads17secbbroadcastreceiver;
 
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent("android.provider.Telephony.SMS_RECEIVED");
+        List<ResolveInfo> infos = getPackageManager().queryBroadcastReceivers(intent, 0);
+        for (ResolveInfo info : infos) {
+            System.out.println("Receiver name:" + info.activityInfo.name + "; priority=" + info.priority);
+        }
+
+
+
+
     }
 }
